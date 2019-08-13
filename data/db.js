224f2ace3bf751daa2d89860e264395e12b0eec1
Chris McDowell <rumbleonuser@RumbleoUsersMBP.lan1>
2019-08-13 14:04:30 -0500

    Initial commit

---
+++ data/db.js
@@ -0,0 +1,14 @@
+const bananaFacts = [
+    {
+        id: 1,
+        fact: '🍌 Bananas are one of the most popular fruits in the American diet 🍌'
+    },
+    {
+        id: 2,
+        fact: '🍌Bananas grow on plants that are officially considered an herb 🍌'
+    },
+    {
+        id: 3,
+        fact: '🍌The banana is actually classified as a berry 🍌'
+    }
+]
