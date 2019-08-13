37f9f6896b4c3fc8ae2944b792b0328f302346ae
Chris McDowell <rumbleonuser@RumbleoUsersMBP.lan1>
2019-08-13 14:46:57 -0500

    added banana facts

--- data/db.js
+++ data/db.js
@@ -10,7 +10,48 @@ const bananaFacts = [
     {
         id: 3,
         fact: '🍌The banana is actually classified as a berry 🍌'
-    }
+    },
+    {
+        id: 4,
+        fact: '🍌Bananas ripen significantly faster when attached to a bunch 🍌'
+    },
+    {
+        id: 5,
+        fact: '🍌A thief in Mumbai was forced to eat 48 bananas so that the gold chain he had swallowed when he was arrested would leave his body. 🍌'
+    },
+    {
+        id: 5,
+        fact: '🍌Bananas are curved because they grow upside-down towards the sun 🍌'
+    },
+    {
+        id: 6,
+        fact: '🍌There are more trade restrictions on bananas than on AK-47s. 🍌'
+    },
+    {
+        id: 7,
+        fact: '🍌Bananas give off radiation 🍌'
+    },
+    {
+        id: 8,
+        fact: '🍌Slipping on banana peels became a comedic staple after banana peels were deemed a threat to public safety in the late 1800s. 🍌'
+    },
+    {
+        id: 9,
+        fact: '🍌Humans share roughly 60% of their DNA with bananas 🍌'
+    },
+    {
+        id: 10,
+        fact: '🍌Despite frequently being portrayed in a sexual way, the most common strain of bananas are completely sterile 🍌'
+    },
+    {
+        id: 11,
+        fact: '🍌Humans share roughly 60% of their DNA with bananas 🍌'
+    },
+    {
+        id: 12,
+        fact: '🍌Humans share roughly 60% of their DNA with bananas 🍌'
+    },
+
 ]
 
 export default bananaFacts
